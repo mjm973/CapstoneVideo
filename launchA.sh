008eab6da06896ab299d53efd0f2a4a465456008
Mateo Juvera <mjm973@nyu.edu>
2018-04-20 21:07:57 +0400

    Processes should now die properly

--- launchA.sh
+++ launchA.sh
@@ -1,2 +1,6 @@
+trap "kill 0" EXIT
+
 python3 server.py &
 python3 keyhole.py --host 10.225.47.90 --path /Volumes/USB/key.txt &
+
+wait
--- launchB.sh
+++ launchB.sh
@@ -1,2 +1,6 @@
+trap "kill 0" EXIT
+
 python3 server.py -b &
 python3 keyhole.py --host 10.225.99.83 --path /Volumes/USB/key.txt &
+
+wait
