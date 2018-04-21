5ad897c3a59726d0281e8c32ac329dfbb0886b9c
Mateo Juvera <mjm973@nyu.edu>
2018-04-21 18:51:20 +0400

    radda radda

--- launchA.sh
+++ launchA.sh
@@ -2,6 +2,6 @@ trap "kill 0" EXIT
 
 python3 server.py &
 python3 keyhole.py --host 10.225.47.90 --path /Volumes/USB/key.txt &
-python3 relay.py -m --thost 10.225.47.90 --tport 8888 --mport 8080 &
+python3 relay.py -m --thost 10.225.47.90 --tport 8888 --mport 8888 &
 
 wait
--- launchB.sh
+++ launchB.sh
@@ -2,6 +2,6 @@ trap "kill 0" EXIT
 
 python3 server.py -b &
 python3 keyhole.py --host 10.225.99.83 --path /Volumes/USB/key.txt &
-python3 relay.py -m --thost 10.225.99.83 --tport 8080 --mport 8888 &
+python3 relay.py -m --thost 10.225.99.83 --tport 8888 --mport 8888 &
 
 wait
