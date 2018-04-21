762241b2f8c444008420472d7e4b6e449ec042e6
Mateo Juvera <mjm973@nyu.edu>
2018-04-21 18:46:07 +0400

    Launchers ready: final test!

--- launchA.sh
+++ launchA.sh
@@ -2,5 +2,6 @@ trap "kill 0" EXIT
 
 python3 server.py &
 python3 keyhole.py --host 10.225.47.90 --path /Volumes/USB/key.txt &
+python3 relay.py -m --thost 10.225.47.90 --tport 8888 --mport 8080 &
 
 wait
