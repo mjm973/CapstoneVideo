acbb4ba9095eb01426b3ad41e7baeba4a12503ae
Mateo Juvera <mjm973@nyu.edu>
2018-04-21 16:01:06 +0400

    Install shell script in case thing screws up

---
+++ install.sh
@@ -0,0 +1,11 @@
+trap "kill 0" EXIT
+
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install python
+pip3 install flask
+pip3 install requests
+pip3 install opencv-python==3.3.0.10
+pip3 install pyserial
+pip3 install python-osc
+
+wait
